fd1e893fbb4f37145bbec2f76b4aad59180591ef
hp <hp@DESKTOP-TD8PHSB>
2024-07-03 15:52:33 -0700

    commit

---
+++ corejavaprograms/src/com/siiet/packages/Encapsulationdemo.java
@@ -0,0 +1,16 @@
+package com.siiet.packages;
+
+public class Encapsulationdemo {
+	int serialnumber;
+	String name;
+	int age;
+	void  show() {
+		System.out.println("serialnumber+","+name+","+)
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ corejavaprograms/src/com/siiet/packages/PublicDemo.java
@@ -0,0 +1,9 @@
+package com.siiet.packages;
+
+public class PublicDemo {
+	private int a=8;
+	public static void main(String[] args) {
+		PublicDemo p=new PublicDemo();
+		System.out.println(p.a);
+	}
+}
